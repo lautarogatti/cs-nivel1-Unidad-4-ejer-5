207ae21980efed753e278b274c53e61364ad6393
lautaro gatti <lautarogatti98@gmail.com>
2024-04-25 21:09:48 -0300

    subida a repositorio

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ejercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Hacer un programa para ingresar 4 números. Luego analizar e informar por pantalla si los mismos se encuentran ordenados de forma decreciente.
+
+            int n1, n2, n3, n4;
+             Console.WriteLine("Ingrese el primer numero");
+             n1 = int.Parse(Console.ReadLine());
+             Console.WriteLine("Ingrese el segundo numero");
+             n2 = int.Parse(Console.ReadLine());
+             Console.WriteLine("Ingrese el tercer numero");
+             n3 = int.Parse(Console.ReadLine());
+             Console.WriteLine("Ingrese el cuarto numero");
+             n4 = int.Parse(Console.ReadLine());
+             if(n1 > n2 && n2 > n3 && n3 > n4){
+                Console.WriteLine("Estan ordenados de forma decreciente");
+             }else{
+                Console.WriteLine("No estan ordenados de forma decreciente");
+             }
+        }
+    }
+}
